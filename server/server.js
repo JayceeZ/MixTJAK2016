2d37ce49b703825cac443df6bebf36b310a0aac5
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2015-12-20 17:53:08 +0100

    Babystep, Micro-Server for serving statics, XHR basic request and API Audio basic example

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('/static', express.static(__dirname + '/public'));
+
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
