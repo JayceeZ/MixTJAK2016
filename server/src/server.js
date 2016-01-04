ba5502d6f481f9e39860d1d1d904108a6848a3bd
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2016-01-04 14:36:46 +0100

    Adapting express for working because it was not

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+var logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    socketio = require('socket.io'),
+    path = require("path"),
+    _ = require('lodash');
+
+/* Express server */
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// LAUNCH SERVER
+app.listen(8000);
